73c10c28a585d0383ff04d19ce369a37c84ab6e6
imbregi <bodhiandrew@mmc.school.nz>
2025-04-10 13:11:09 +1200

    Function to create decorations on single line headings
    Function to create decorations on single line headings

---
+++ C_01_Make_Statement.py
@@ -0,0 +1,8 @@
+# Functions go here
+def make_statement(statement, decoration):
+    """Emphasises headings by adding decorationg to the start and end"""
+
+    print(f"{decoration * 3} {statement} {decoration * 3}")
+
+# Main routine goes here
+make_statement("Programming is fun!", "👍")
